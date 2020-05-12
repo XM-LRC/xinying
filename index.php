bce74b8357c3b13dcff5173fca3a16bc037d993f
林镕琛 <linrongchen_xm@sina.com>
2020-05-12 15:41:23 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 	
 	//处理登录
@@ -33,10 +34,65 @@ if(isset($_POST['dosubmit'])) {
 
 ?>
 
-<form action="index.php" method="post">
-	用户名: <input type="text" name="username" value="张三" /><br>
-	密码: <input type="password" name="password" value="123456" /><br>
+<!DOCTYPE html>
+<html lang="en">
 
-	<input type="submit" name="dosubmit" value="提交"> <br>
-</form>
-<a href="index.php">返回</a><br>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Spica Admin</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller d-flex">
+    <div class="container-fluid page-body-wrapper full-page-wrapper d-flex">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="images/logo.svg" alt="logo">
+              </div>
+              <h4>新影--可视化分析</h4>
+              <h6 class="font-weight-light">登录系统</h6>
+              <form class="pt-3" action="index.php" method="post">
+                <div class="form-group">
+                  <input type="type" name="username" value="张三" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+	
+	
+                </div>
+                <div class="form-group">
+                  <input type="password" name="password" value="123456"  class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="mt-3">
+	<input type="submit" name="dosubmit" value="登录" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"> <br>
+                </div>
+             
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- base:js -->
+
+  <!-- endinject -->
+</body>
+
+</html>
+
--- userinfor.php
+++ userinfor.php
@@ -1,3 +1,142 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>登录信息</title>
+        <link rel="stylesheet" href="样式.css">
+        <script src="echarts.min.js"></script>
+        <script src="vintage.js"></script><!-- 复古风格 -->
+        <!-- <script src="dark.js"></script> -->
+	</head>
+	<body>
+			<div id='logo'>
+               
+                    <span id="zi"> 新影--数据可视化</span> 
+				
+            </div>
+		
+	<?php
+	include "connect.php";
+	include "config.inc.php";
+
+	
+/*	echo '<br>';
+	echo '<br>';*/
+/************************下面这一个块实现搜索：观看记录*******************************/
+	$whr=array();
+		$whr[]=(" uid={$_SESSION['uid']}") ;
+	if(isset($_GET['action']) && $_GET['action']=='ser') {
+		$tmp = !empty($_POST) ? $_POST : $_GET;
+		$args = "";
+		
+		if(!empty($tmp['uid'])) {
+			$whr[] = "uid like '%{$tmp['uid']}%'";
+
+			$args .= "&uid={$tmp['uid']}";
+		}
+		
+
+		//按照大类搜索，
+		if(!empty($tmp['bigtype'])) {
+			$whr[] = "bigtype = '{$tmp['bigtype']}'";
+			$args .= "&bigtype={$tmp['bigtype']}";
+		}
+
+		//按照小类搜索
+		if(!empty($tmp['smalltype'])) {
+			$whr[] = "smalltype = '{$tmp['smalltype']}'";
+			$args .= "&smalltype={$tmp['smalltype']}";
+		}
+
+		//按照影视作品搜索
+		if(!empty($tmp['watchname'])) {
+			$whr[] = "watchname = '{$tmp['watchname']}'";
+			$args .= "&watchname={$tmp['watchname']}";
+		}
+		
+
+		if(!empty($whr)) {
+			$where = " where ".implode(" and ", $whr);
+		} else {
+			
+			$where = "";
+		}
+	}
+		if(!empty($whr)) {
+			$where = " where ".implode(" and ", $whr);
+		} else {
+			
+			$where = "";
+		}
+	
+/************************数据库查询*******************************/	
+	$sql = "select uid, bigtype, smalltype, watchname from tb_userwatch {$where} ";
+	$stmt = $db -> prepare($sql);
+	$stmt -> execute();
+
+	/*echo "<h2>搜索条件</h2>";
+	echo '<form action="userinfor.php?action=ser" method="post">';
+	echo '按uid:<input type="text" name="uid" size=8 value="'.$tmp['uid'].'">&nbsp;&nbsp;';
+
+	echo '按大类:<input type="text" name="bigtype" size=8  value="'.$tmp['bigtype'].'">&nbsp;&nbsp;';
+	echo '按小类: <input type="text" size="8" name="smalltype"  value="'.$tmp['smalltype'].'">&nbsp;&nbsp;';
+	echo '按名称 <input type="text" size="8" name="watchname"  value="'.$tmp['watchname'].'">&nbsp;&nbsp;';
+	echo '<input type="submit" name="sersubmit" value="搜索">';
+	echo '</form>';
+	echo '<br>';
+	echo '<br>';
+	echo '<br>';*/	
+		
+		?>
+		
+		
+            <div id="lcontent">
+
+                <div id='main'>
+                    <div id="main1">
+						<?php
+                      echo' <span id="informain"> '.'姓名:  '. $_SESSION['username']. '&nbsp  &nbsp  &nbsp'.'性别:  '. $_SESSION['sex']. '&nbsp  &nbsp  &nbsp'.'年龄:  '. $_SESSION['age']. '&nbsp  &nbsp  &nbsp'.'职业:  '. $_SESSION['job']. '&nbsp  &nbsp  &nbsp'. '是否会员:  '. $_SESSION['vip']. '&nbsp  &nbsp  &nbsp'. '观影时长:  '. $_SESSION['time']. '小时'.'&nbsp  &nbsp  &nbsp'.'</span>';
+						?>
+                    </div>
+					
+					
+                    <div id="main2">
+                        <table align="center" width="900" height="50" border="3px" cellpadding="1" cellspacing="1"  frame=hsides rules="rows">
+                            <caption><h2 style="color:black"> 张三的观影历史</h2> </caption>
+                            <thead align="center" bgcolor="#FFCCFF" valign="center">
+                                <tr>
+                                   
+                                    <td width="76"> 影视大类</td>
+                                    <td width="76"> 所属小类 </td>
+                                    <td width="76"> 影视名称 </td>
+                                    <td width="76"> 导演名 </td>
+                                    <td width="82"> 上架日期 </td>
+                                </tr>
+                            </thead>
+                          <?php  echo '<tbody align="center" bgcolor="#FFCCFF" valign="center">';
+                               while(list( $uid, $bigtype,$smalltype, $watchname) = $stmt->fetch(PDO::FETCH_NUM)) {
+                                   
+                                    echo '<tr>';
+									
+									echo '<td width="76">'.$bigtype.'</td>';
+									echo '<td width="76">'.$smalltype.'</td>';
+									echo '<td width="76">'.$watchname.'</td>';
+							   }
+                                
+                           echo' </tbody>';
+									?>
+                            <tfoot align="center" bgcolor="#9999CC" valign="middle">
+                               
+                            </tfoot>
+                            </table>
+                    </div>
+                </div>
+
+            </div>
+        
+	</body>
+</html>
+
 <?php
 	include "connect.php";
 	include "config.inc.php";
